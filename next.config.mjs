5de626054039eb71cff9e0a63f0f603452bde2b5
Shivani Balaji <shivanibalaji2612@gmail.com>
2024-10-02 01:08:35 +0530

    preparing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    output: 'export',
+    assetPrefix: './',
+    images: {
+      unoptimized: true,
+    },
+  };
+
 export default nextConfig;
