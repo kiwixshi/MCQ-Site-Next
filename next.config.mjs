52a9e23c4dcebf83bfe54a9f9849234d0da9c0fa
Shivani Balaji <shivanibalaji2612@gmail.com>
2024-10-05 17:16:32 +0530

    error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    output: 'export',
-    assetPrefix: './',
-    images: {
-      unoptimized: true,
-    },
-  };
 
 export default nextConfig;
