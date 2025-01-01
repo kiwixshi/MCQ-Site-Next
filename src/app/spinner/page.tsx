97c2c6f9d9174cccc45900d592c1db92f9125fd6
Shivani Balaji <shivanibalaji2612@gmail.com>
2025-01-01 23:55:28 +0300

    .

---
+++ src/app/spinner/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React, {FC} from 'react';
+import Spinner from './spinner';
+
+
+const Page : FC = () => {
+    return (
+        <Spinner></Spinner>
+    )
+}
+
+export default Page;
